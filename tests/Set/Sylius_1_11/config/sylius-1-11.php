d5cd18bd2952fde01a3d12c48ad6f3b24867618f
Alexander Schranz <alexander@sulu.io>
2022-05-30 10:16:01 +0200

    Initialize Sylius Rector

---
+++ tests/Set/Sylius_1_11/config/sylius-1-11.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Sylius\Rector\Set\SyliusSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/../../../../config/config.php');
+    $rectorConfig->sets([SyliusSetList::SYLIUS_1_11]);
+};
